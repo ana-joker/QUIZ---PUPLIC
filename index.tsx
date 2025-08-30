1ca296417c5d1314cce2e758ef7911a086140ff2
Ahmed Elenany <ahmedelgoo9918@gmail.com>
2025-08-30 14:33:39 +0300

    Re-apply index.js with CORS fix and ensure syntax correctness

--- index.tsx
+++ index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import './index.css';
+import './src/index.css';
 
 
 const rootElement = document.getElementById('root');
